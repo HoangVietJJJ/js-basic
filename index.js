9832a3ab7e07395f6820339731a9afee86b416a1
HoangViet <hoangphamhl000@gmail.com>
2024-02-20 15:44:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log(`Hello world`);
+// console.log(`javascript`);
+
+// window.alert(`Alert`);
+// window.alert(`Alert Alert`);
+
+document.getElementById(`myH1`).textContent = 'Hello';
+document.getElementById(`myP`).textContent = 'Hello World';
+
+//note
